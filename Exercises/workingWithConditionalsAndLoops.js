95709c43b3b9b80a64423bfb5be78695ca41bd05
Mattx2k1 <therealmattharris@gmail.com>
2023-02-20 19:14:31 -0500

    added exercise

--- Exercises/workingWithConditionalsAndLoops.js
+++ Exercises/workingWithConditionalsAndLoops.js
@@ -208,4 +208,23 @@ switch (i) {
 
 5. Still not working. Taking a break and will investigate later
 
+6. This is the correct answer:
+
+for (var i = 1; i <= 10; i++) {
+    switch(i) {
+        case 1:
+            console.log("Gold medal")
+            break
+        case 2:
+            console.log("Silver medal")
+            break
+        case 3:
+            console.log("Bronze medal")
+            break
+        default:
+            //this block will run if no condition matches
+            console.log(i)
+    }
+}
+
 */
