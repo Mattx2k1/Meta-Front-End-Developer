af789df4efc039bb1b564f10da684f69c2aba28c
Mattx2k1 <therealmattharris@gmail.com>
2023-04-18 14:48:19 -0400

    Task 1 complete

--- Labs/building-a-functional-program/home/coder/project/learn/functionalprogramming.js
+++ Labs/building-a-functional-program/home/coder/project/learn/functionalprogramming.js
@@ -1,6 +1,11 @@
 // Task 1: Build a function-based console log message generator
-function consoleStyler() {
-    
+function consoleStyler(color, background, fontSize, txt) {
+    let message = "%c" + txt;
+    let style = `color: ${color};`
+    style += `background: ${background};`
+    style += `font-size: ${fontSize};
+    `
+    console.log(message);
 }
 
 // Task 2: Build another console log message generator
