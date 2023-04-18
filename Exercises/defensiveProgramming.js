7e1385168fd5e06d14eb4c07ed736ef2800e621b
Mattx2k1 <therealmattharris@gmail.com>
2023-04-18 11:18:21 -0400

    completed exercise; added solution

--- Exercises/defensiveProgramming.js
+++ Exercises/defensiveProgramming.js
@@ -51,25 +51,147 @@
 
 // Declare a variable named condition2 on the next line and assign to it and assign to it a check that makes sure that the type of match is a string AND that the length of the match variable is equal to 1.
 
-function letterFinder(word, match) {
-    let condition1 = typeof(word) == 'string' && word.length >= 2;
-    let condition2 = typeof(match) == 'string' && word.length == 1;
-    for(i = 0; i < word.length; i++) {
-        if(word[i] == match) {
-            //if the current character at position i in the word is equal to the match
-            console.log('Found the', match, 'at', i)
-        } else {
-            console.log('---No match found at', i)
-        }
-    }
-}
+// function letterFinder(word, match) {
+//     let condition1 = typeof(word) == 'string' && word.length >= 2;
+//     let condition2 = typeof(match) == 'string' && word.length == 1;
+//     for(i = 0; i < word.length; i++) {
+//         if(word[i] == match) {
+//             //if the current character at position i in the word is equal to the match
+//             console.log('Found the', match, 'at', i)
+//         } else {
+//             console.log('---No match found at', i)
+//         }
+//     }
+// }
 
 // Write an if statement on the next line that checks that condition1 is true, and condition2 is true
 
+// function letterFinder(word, match) {
+//     let condition1 = typeof(word) == 'string' && word.length >= 2;
+//     let condition2 = typeof(match) == 'string' && word.length == 1;
+//     if (condition1 == true && condition2 == true) {
+
+//     }
+//     for(i = 0; i < word.length; i++) {
+//         if(word[i] == match) {
+//             //if the current character at position i in the word is equal to the match
+//             console.log('Found the', match, 'at', i)
+//         } else {
+//             console.log('---No match found at', i)
+//         }
+//     }
+// }
+
 // Move the rest of the function's body into the if statement you wrote in the previous step.
+
+// function letterFinder(word, match) {
+//     let condition1 = typeof(word) == 'string' && word.length >= 2;
+//     let condition2 = typeof(match) == 'string' && word.length == 1;
+//     if (condition1 == true && condition2 == true) {
+//         for(i = 0; i < word.length; i++) {
+//         if(word[i] == match) {
+//             //if the current character at position i in the word is equal to the match
+//             console.log('Found the', match, 'at', i)
+//         } else {
+//             console.log('---No match found at', i)
+//         }
+//     }
+//     }
+// }
 
 // Code an "else" block after the "if" condition and console.log the following: "Please pass correct arguments to the function.".
 
+// function letterFinder(word, match) {
+//     let condition1 = typeof(word) == 'string' && word.length >= 2;
+//     let condition2 = typeof(match) == 'string' && word.length == 1;
+//     if (condition1 == true && condition2 == true) {
+//         for(i = 0; i < word.length; i++) {
+//         if(word[i] == match) {
+//             //if the current character at position i in the word is equal to the match
+//             console.log('Found the', match, 'at', i)
+//         } else {
+//             console.log('---No match found at', i)
+//         }
+//     }
+//     } else console.log("Please pass correct arguments to the function.")
+// }
+
 // As a failing test, run the letterFinder function and pass it with any two numbers as arguments.
 
+// function letterFinder(word, match) {
+//     let condition1 = typeof(word) == 'string' && word.length >= 2;
+//     let condition2 = typeof(match) == 'string' && word.length == 1;
+//     if (condition1 == true && condition2 == true) {
+//         for(i = 0; i < word.length; i++) {
+//         if(word[i] == match) {
+//             //if the current character at position i in the word is equal to the match
+//             console.log('Found the', match, 'at', i)
+//         } else {
+//             console.log('---No match found at', i)
+//         }
+//     }
+//     } else console.log("Please pass correct arguments to the function.")
+// }
+
+// letterFinder(1,2);
+
 // As a passing test, run the letterFinder funciton and pass it with correct arguments, such as: letterFinder("cat", "c").
+
+// function letterFinder(word, match) {
+//     let condition1 = typeof(word) == 'string' && word.length >= 2;
+//     let condition2 = typeof(match) == 'string' && word.length == 1;
+//     if (condition1 == true && condition2 == true) {
+//         for(i = 0; i < word.length; i++) {
+//         if(word[i] == match) {
+//             //if the current character at position i in the word is equal to the match
+//             console.log('Found the', match, 'at', i)
+//         } else {
+//             console.log('---No match found at', i)
+//         }
+//     }
+//     } else console.log("Please pass correct arguments to the function.")
+// }
+
+// letterFinder(1,2);
+// letterFinder("cat", "c");
+
+// function letterFinder(word, match) {
+//     let condition1 = typeof(word) == String && word.length >= 2;
+//     let condition2 = typeof(match) == String && word.length == 1;
+//     if (condition1 == true && condition2 == true) {
+//         for(i = 0; i < word.length; i++) {
+//         if(word[i] == match) {
+//             //if the current character at position i in the word is equal to the match
+//             console.log('Found the', match, 'at', i)
+//         } else {
+//             console.log('---No match found at', i)
+//         }
+//     }
+//     } else console.log("Please pass correct arguments to the function.")
+// }
+
+// letterFinder(1, 2);
+// letterFinder("cat", "c");
+
+// Correct solution
+
+function letterFinder(word, match) {
+    var condition1 = typeof(word) == 'string' && word.length >= 2; //if the word is a string and the length is greater than or equal to 2
+    var condition2 = typeof(match) == 'string' && match.length == 1; //if the match is a string and the length is equal to 1
+    if(condition1 && condition2) { //if both condition matches
+        for(var i = 0; i < word.length; i++) {
+            if(word[i] == match) {
+                //check if the character at this i position in the word is equal to the match
+                console.log('Found the', match, 'at', i)
+            } else {
+                console.log('---No match found at', i)
+            }
+        }
+    } else {
+        //if the requirements don't match
+        console.log("Please pass correct arguments to the function")
+    }
+}
+letterFinder([],[])
+letterFinder(1,2)
+letterFinder("cat","c")
