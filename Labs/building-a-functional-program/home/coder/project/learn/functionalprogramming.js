418f14c79c1e66f1a074b268feb0b527d8287088
Mattx2k1 <therealmattharris@gmail.com>
2023-05-07 13:17:22 -0400

    made some edits; passed 100%

--- Labs/building-a-functional-program/home/coder/project/learn/functionalprogramming.js
+++ Labs/building-a-functional-program/home/coder/project/learn/functionalprogramming.js
@@ -1,20 +1,20 @@
 // Task 1: Build a function-based console log message generator
 function consoleStyler(color, background, fontSize, txt) {
-    let message = "%c" + txt;
-    let style = `color: ${color};`
+    var message = "%c" + txt;
+    var style = `color: ${color};`
     style += `background: ${background};`
     style += `font-size: ${fontSize};`
-    console.log(message + style);
+    console.log(message, style);
 }
 
 // Task 2: Build another console log message generator
 function celebrateStyler(reason) {
-    let fontStyle = "color: tomotato; font-size: 50px"
+    var fontStyle = "color: tomotato; font-size: 50px";
     if (reason == "birthday") {
-        console.log(`%cHappy birthday"`, fontStyle);
+        console.log(`%cHappy birthday`, fontStyle);
     } else if (reason == "champions") {
-        console.log(`%Congrats on the title!`, fontStyle);
-    } else console.log(message, style);
+        console.log(`%cCongrats on the title!`, fontStyle);
+    } else console.log(reason, fontStyle);
 } 
 
 // Task 3: Run both the consoleStyler and the celebrateStyler functions
@@ -23,7 +23,7 @@ consoleStyler('#1d5c63', '#ede6db', '40px', 'Congrats!');
 celebrateStyler('birthday');
 
 // Task 4: Insert a congratulatory and custom message
-function styleAndCelebrate() {
+function styleAndCelebrate(color, background, fontSize, txt, reason) {
     consoleStyler(color, background, fontSize, txt);  
     celebrateStyler(reason);
 }
