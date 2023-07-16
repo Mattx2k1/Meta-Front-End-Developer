f21d2680823251d89f0ea13f91ad8f0777faef01
Mattx2k1 <therealmattharris@gmail.com>
2023-07-16 18:57:10 -0400

    task 1 completed

--- Labs/building-an-objected-oritented-program/home/coder/project/learn/ooprogramming.js
+++ Labs/building-an-objected-oritented-program/home/coder/project/learn/ooprogramming.js
@@ -1,5 +1,21 @@
 // Task 1: Code a Person class
 
+class Person {
+    constructor(name = "tom", age = 20, energy = 100) {
+        this.name = name;
+        this.age = age;
+        this.energy = energy;
+    }
+    sleep() {
+        this.energy += 10;
+
+    }
+    doSomethingFun() {
+        this.energy -= 10;
+
+    }
+}
+
 // Task 2: Code a Worker class
 
 // Task 3: Code an intern object, run methods
