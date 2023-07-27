25151633c81c7c27f8c98dabe576a378e202be83
Mattx2k1 <therealmattharris@gmail.com>
2023-07-26 21:17:03 -0400

    task 1 completed

--- Projects/Programming Assignment-Array and object iteration/home/coder/project/learn/solution.js
+++ Projects/Programming Assignment-Array and object iteration/home/coder/project/learn/solution.js
@@ -1,6 +1,19 @@
 // Task 1
 
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
+
+function logDairy(foodgroup) {
+    for (var i of foodgroup) {
+        console.log(i);
+    }
+
+}
+
+logDairy(dairy);
+
 // Task 2
+
+
 
 // Task 3
 
