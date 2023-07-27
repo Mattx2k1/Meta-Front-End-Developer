e8fc5602d0e8b4d15bfc2713227bb528fd6a2dcb
Mattx2k1 <therealmattharris@gmail.com>
2023-07-26 21:18:34 -0400

    task 2 starter code added

--- Projects/Programming Assignment-Array and object iteration/home/coder/project/learn/solution.js
+++ Projects/Programming Assignment-Array and object iteration/home/coder/project/learn/solution.js
@@ -13,7 +13,17 @@ logDairy(dairy);
 
 // Task 2
 
+const animal = {
 
+    canJump: true
+
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
 
 // Task 3
 
