9e0760d1dcabd653c27b1c0830bdcd73f1d015f8
Mattx2k1 <therealmattharris@gmail.com>
2023-08-05 10:10:58 -0400

    getting expected output but still failing

--- Projects/Programming Assignment-Array and object iteration/home/coder/project/learn/solution.js
+++ Projects/Programming Assignment-Array and object iteration/home/coder/project/learn/solution.js
@@ -3,8 +3,8 @@
 var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
 
 function logDairy(foodgroup) {
-    for (var i of foodgroup) {
-        console.log(i);
+    for (key of foodgroup) {
+        console.log(key);
     }
 
 }
@@ -35,3 +35,32 @@ birdCan(bird);
 
 // Task 3
 
+function animalCan(animalObject, animalPrototype) {
+    for([key, value] of Object.entries(animalObject)) {
+        console.log(`${key}: ${value}`);
+    }
+    for([key, value] of Object.entries(animalPrototype)) {
+        console.log(`${key}: ${value}`);
+    }
+}
+
+animalCan(bird, animal);
+
+/* 
+Got these errors before changes:
+
+FAILED: Console logged expected values for logDairy - returned TypeError: foodgroup is not iterable but expected cheesesour creammilkyogurtice creammilkshake
+FAILED: Console logged expected values for birdCan - returned TypeError: Cannot convert undefined or null to object but expected canFly: truehasFeathers: true
+FAILED: Console logged expected values for animalCan - returned TypeError: Cannot convert undefined or null to object but expected canFly: truehasFeathers: truecanJump: true
+
+*/
+
+// New errors after second attempt:
+
+/*
+
+FAILED: Console logged expected values for logDairy - returned TypeError: foodgroup is not iterable but expected cheesesour creammilkyogurtice creammilkshake
+FAILED: Console logged expected values for birdCan - returned TypeError: Cannot convert undefined or null to object but expected canFly: truehasFeathers: true
+FAILED: Console logged expected values for animalCan - returned TypeError: Cannot convert undefined or null to object but expected canFly: truehasFeathers: truecanJump: true
+
+*/
