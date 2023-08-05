a43fbb5dc2ccc093b36d7e2aceda6d8148222e17
Mattx2k1 <therealmattharris@gmail.com>
2023-08-05 11:08:13 -0400

    removed function parameters and changed loop parameters

--- Projects/Programming Assignment-Array and object iteration/home/coder/project/learn/solution.js
+++ Projects/Programming Assignment-Array and object iteration/home/coder/project/learn/solution.js
@@ -25,8 +25,8 @@ bird.canFly = true;
 
 bird.hasFeathers = true;
 
-function birdCan(animalObject) {
-    for ([key, value] of Object.entries(animalObject)) {
+function birdCan(bird) {
+    for ([key, value] of Object.entries(bird)) {
         console.log(`${key}: ${value}`);
     }
 }
@@ -35,32 +35,13 @@ birdCan(bird);
 
 // Task 3
 
-function animalCan(animalObject, animalPrototype) {
-    for([key, value] of Object.entries(animalObject)) {
+function animalCan(bird, animal) {
+    for([key, value] of Object.entries(bird)) {
         console.log(`${key}: ${value}`);
     }
-    for([key, value] of Object.entries(animalPrototype)) {
+    for([key, value] of Object.entries(animal)) {
         console.log(`${key}: ${value}`);
     }
 }
 
 animalCan(bird, animal);
-
-/* 
-Got these errors before changes:
-
-FAILED: Console logged expected values for logDairy - returned TypeError: foodgroup is not iterable but expected cheesesour creammilkyogurtice creammilkshake
-FAILED: Console logged expected values for birdCan - returned TypeError: Cannot convert undefined or null to object but expected canFly: truehasFeathers: true
-FAILED: Console logged expected values for animalCan - returned TypeError: Cannot convert undefined or null to object but expected canFly: truehasFeathers: truecanJump: true
-
-*/
-
-// New errors after second attempt:
-
-/*
-
-FAILED: Console logged expected values for logDairy - returned TypeError: foodgroup is not iterable but expected cheesesour creammilkyogurtice creammilkshake
-FAILED: Console logged expected values for birdCan - returned TypeError: Cannot convert undefined or null to object but expected canFly: truehasFeathers: true
-FAILED: Console logged expected values for animalCan - returned TypeError: Cannot convert undefined or null to object but expected canFly: truehasFeathers: truecanJump: true
-
-*/
