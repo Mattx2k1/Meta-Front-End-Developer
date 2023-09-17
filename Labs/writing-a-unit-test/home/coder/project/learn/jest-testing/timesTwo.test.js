bf207551d2911d091cb4604cdefd055871b46a48
Mattx2k1 <therealmattharris@gmail.com>
2023-09-17 18:23:34 -0400

    task 4 almost complete; wrote the test and returning results in terminal. failing tests

--- Labs/writing-a-unit-test/home/coder/project/learn/jest-testing/timesTwo.test.js
+++ Labs/writing-a-unit-test/home/coder/project/learn/jest-testing/timesTwo.test.js
@@ -1,6 +1,6 @@
 const timesTwo = require('./timesTwo');
 
 // Write the first test
-test('returns ...', () => {
-    expect().toBe()
+test('returns the number times 2', () => {
+    expect(10).toBe(20)
 });
