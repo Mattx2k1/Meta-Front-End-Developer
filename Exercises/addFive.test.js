4b58bf97812f1253a0781019bf141656f2aacfae
Mattx2k1 <therealmattharris@gmail.com>
2023-09-17 12:40:49 -0400

    updated exercise files as instructed. Reinstalled Jest locally

--- Exercises/addFive.test.js
+++ Exercises/addFive.test.js
@@ -1,2 +1,6 @@
-const addFive = require(`./addFive`)
+const { default: TestRunner } = require("jest-runner");
+const addFive = require(`./addFive`);
 
+test('returns the number plus 5', () => {
+    expect(addFive(1)).toBe(6);
+})
