7e34ca8df41240f1a091ff58bccbc2d542444f7b
Mattx2k1 <therealmattharris@gmail.com>
2023-12-21 21:11:47 -0500

    removed backslash n, not sure why I had it in the first place

--- Assignments/Little Lemon Receipt Maker/home/coder/project/learn/finalProject.js
+++ Assignments/Little Lemon Receipt Maker/home/coder/project/learn/finalProject.js
@@ -24,7 +24,7 @@ function getPrices(taxBoolean) {
     for (let i = 0; i < dishData.length; i++) {
         let finalPrice;
         if (taxBoolean === true) {
-            finalPrice = ((dishData[i].price) * tax);
+            finalPrice = (dishData[i].price * tax);
 
         } else if (taxBoolean === false) {
             finalPrice = dishData[i].price;
@@ -33,7 +33,7 @@ function getPrices(taxBoolean) {
             return; 
             
         }
-        console.log(`Dish: ${dishData[i].name} Price: $${finalPrice}\n`);
+        console.log(`Dish: ${dishData[i].name} Price: $${finalPrice}`);
     } 
     
 }
@@ -57,7 +57,16 @@ function getDiscount(taxBoolean, guests) {
 
 getDiscount(true, 2);
 getDiscount(false, 10);
-getDiscount();
+getDiscount(true, 0);
+getDiscount(true, 1);
+getDiscount(true, 2);
+getDiscount(true, 3);
+getDiscount(true, 4);
+getDiscount(true, 5);
+getDiscount(true, 10);
+getDiscount(true, 15);
+getDiscount(true, 20);
+getDiscount(true, 30);
 getDiscount(true, 100);
 getDiscount(false, 100);
 getDiscount('a', 'b');
